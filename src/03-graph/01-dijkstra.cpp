b5f39ae0a4b41ea1b49b92e59bcb9bb139fc7fd5
Ahmad Abdulrahman <ah.abdulrahmaan@gmail.com>
2026-07-19 21:33:55 +0400

    use edge struct with custom operator

--- src/03-graph/01-dijkstra.cpp
+++ src/03-graph/01-dijkstra.cpp
@@ -1,5 +1,14 @@
 int n;
 const long long LINF = 0x3f3f3f3f3f3f3f3fLL;
+struct Edge {
+    int u, v;
+    long long w;
+
+    // reversed so the default priority_queue (max-heap) pops the smallest weight
+    bool operator < (const Edge& o) const {
+        return w > o.w;
+    }
+};
 vector<vector<pair<int, long long>>> adj;
 vector<long long> dist;
 
@@ -8,20 +17,20 @@ vector<long long> dist;
  */
 void dijkstra(int src) {
     dist.assign(n, LINF);
-    priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<>> pq;
+    priority_queue<Edge> pq;
 
     dist[src] = 0;
-    pq.push({0, src});
+    pq.push({src, src, 0});
 
     while (not pq.empty()) {
-        auto [d, u] = pq.top();
+        Edge cur = pq.top();
         pq.pop();
-        if (d > dist[u]) continue;
+        if (cur.w > dist[cur.v]) continue;
 
-        for (auto [v, w]: adj[u])
-            if (dist[u] + w < dist[v]) {
-                dist[v] = dist[u] + w;
-                pq.push({dist[v], v});
+        for (auto [v, w]: adj[cur.v])
+            if (cur.w + w < dist[v]) {
+                dist[v] = cur.w + w;
+                pq.push({cur.v, v, dist[v]});
             }
     }
 }
